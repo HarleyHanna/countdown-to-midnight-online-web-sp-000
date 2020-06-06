5d6092d46d4410aa549102b517aeb7f00e292324
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-06 12:30:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,9 +5,11 @@ def countdown (seconds)
     seconds.to_s
     puts "#{seconds} SECOND(S)!"
     seconds -= 1
+    countdown_with_sleep()
   end
   return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep (seconds)
-  
+  sleep(1.seconds)
+end
